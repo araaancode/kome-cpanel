56a9d123f12bfd0e5afa884616e7d1ac90e7c09a
araaancode <araaancode@gmail.com>
2024-11-11 12:37:40 +0330

    user authentication finished

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -27,22 +27,51 @@ function sendEmail(data, link) {
 }
 
 const sendSuccessEmail = (data, link) => {
+    // console.log(data);
+
+    // let transporter = nodeMailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         // should be replaced with real sender's account
+    //         user: process.env.EMAIL,
+    //         pass: process.env.EMAIL_PASSWORD,
+    //     },
+    // })
+    // let mailOptions = {
+    //     // should be replaced with real recipient's account
+    //     to: data.email,
+    //     subject: ' گذرواژه با موفقیت تغییر یافت',
+        // text: `سلام ${data.name}  گذرواژه با موفقیت تغییر یافت. روی لینک زیر کلیک کنید تا وارد سایت شوید`,
+        // html: link,
+    // }
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     }
+    //     console.log('Message %s sent: %s', info.messageId, info.response)
+    // })
+
+    console.log(data);
+    console.log(link);
+    
+
     let transporter = nodeMailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
             // should be replaced with real sender's account
-            data: process.env.EMAIL,
+            user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD,
         },
     })
     let mailOptions = {
         // should be replaced with real recipient's account
         to: data.email,
-        subject: ' گذرواژه با موفقیت تغییر یافت',
-        text: `سلام ${data.name}  گذرواژه با موفقیت تغییر یافت. روی لینک زیر کلیک کنید تا وارد سایت شوید`,
-        html: link,
+        subject: 'تغییر گذرواژه',
+        html: `سلام ${data.name}  گذرواژه با موفقیت تغییر یافت. روی لینک زیر کلیک کنید تا وارد سایت شوید` + '<br >' + link,
     }
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
